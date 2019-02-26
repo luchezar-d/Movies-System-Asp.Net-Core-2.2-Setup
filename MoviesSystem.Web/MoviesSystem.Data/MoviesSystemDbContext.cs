a2d3fa86c7a772913729d304ea72ab06ccb27141
Luchezar <Luchezar.d.dimitrov@gmail.com>
2019-02-26 18:17:53 +0200

    Updated The Project

---
+++ MoviesSystem.Web/MoviesSystem.Data/MoviesSystemDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MoviesSystem.Data.Models;
+
+namespace MoviesSystem.Data
+{
+    public class MoviesSystemDbContext : IdentityDbContext<User>
+    {
+        public MoviesSystemDbContext(DbContextOptions<MoviesSystemDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
